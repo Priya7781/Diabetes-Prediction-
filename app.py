d59327ac355fa51a5ee1c8f82872d619f72d536e
Priya <priyadarshinayak2001@gmail.com>
2022-11-01 12:49:09 +0530

    done

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template,request
+import pickle
+
+app = Flask(__name__)
+
+
+@app.route('/',methods=['POST','GET'])
+def home():
+    if request.method == "POST":
+        FS=int(request.form["FS"])
+        FU=int(request.form["FU"])
+        with open('my_model','rb') as f:
+           model = pickle.load(f)    
+        result=model.predict([[FS,FU]])
+        if result[0]=="YES":
+            return render_template('home.html',data=["Sorry you might have diabeties","red"])
+        else:
+            return render_template('home.html',data=["Congratulations You dont have diabetes","green"])    
+    else:
+        return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/profile/<name>')
+def profile(name):
+    return "Hello "+str(name) 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
